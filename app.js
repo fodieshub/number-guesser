f8cb3a1984a88e2f67f50c31bc46c11b9b7e338d
Fodieshub <buzkenny@gmail.com>
2021-01-07 09:40:43 +0100

    declear game value, UI element, listen for click event and input validation

--- app.js
+++ app.js
@@ -4,9 +4,35 @@ Player must get a certain number of guesses
 Notify player of guesses remaining
 Let player choose to play again
 */
-//Game Value
 
+//Game Value
 let min = 1,
     max = 10,
     winningNum = 2,
     guessesLeft = 3;
+
+// UI Element
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessInput = document.querySelector('#guess-input'),
+      guessBtn = document.querySelector('#guess-btn')
+      message = document.querySelector('.message');
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  
+  //Validation
+  if(guess === NaN || guess < min || guess > max){
+    setMessage(`Please Enter a Number Between ${min} and ${max}`);
+  }
+});
+//Error Message
+function setMessage(msg){
+  message.textContent = msg;
+}
+  
