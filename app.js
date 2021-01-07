4113e3f64e8cb3bc5c62c3c05dfbf04f0cc87389
Fodieshub <buzkenny@gmail.com>
2021-01-07 08:37:01 +0100

    Add skeleton cdn and classes for our UI

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+Player must guess a number between the min number and the max number
+Player must get a certain number of guesses
+Notify player of guesses remaining
+Let player choose to play again
+*/
+//Game Value
+
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
